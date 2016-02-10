22ff3e78001e5b1608047569ead080a04afd4aca
Paul Rogero <paulrogero@gmail.com>
2016-02-10 17:12:36 -0500

    V1 done.

---
+++ Woeber.RxExtensions.Tests/KeyedBufferTests.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Subjects;
+using FluentAssertions;
+using Microsoft.Reactive.Testing;
+using Xunit;
+
+namespace Woeber.RxExtensions.Tests
+{
+    public class KeyedBufferTests
+    {
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void SimpleDelay()
+        {
+            var receivedSignals = new List<int>();
+            var subject = new Subject<int>();
+            var scheduler = new TestScheduler();
+            subject.UniqueThrottle(TimeSpan.FromTicks(25), scheduler).Subscribe(x => receivedSignals.Add(x));
+
+
+            subject.OnNext(1);
+            receivedSignals.Count.Should().Be(0);
+
+            scheduler.AdvanceTo(24);
+            receivedSignals.Count.Should().Be(0);
+
+            scheduler.AdvanceTo(25);
+            receivedSignals.Count.Should().Be(1);
+
+            scheduler.AdvanceTo(100);
+            receivedSignals.Count.Should().Be(1);
+        }
+
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void SimpleDelayWith2Inputs()
+        {
+            var receivedSignals = new List<int>();
+            var subject = new Subject<int>();
+            var scheduler = new TestScheduler();
+            subject.UniqueThrottle(TimeSpan.FromTicks(25), scheduler).Subscribe(x => receivedSignals.Add(x));
+
+
+            subject.OnNext(1);
+            receivedSignals.Count.Should().Be(0);
+
+            scheduler.AdvanceTo(10);
+            subject.OnNext(2);
+            subject.OnNext(2);
+            subject.OnNext(2);
+            subject.OnNext(1);
+            subject.OnNext(1);
+            subject.OnNext(1);
+
+            scheduler.AdvanceTo(24);
+            receivedSignals.Count.Should().Be(0);
+
+            scheduler.AdvanceTo(25);
+            receivedSignals.Count.Should().Be(1);
+
+            scheduler.AdvanceTo(34);
+            subject.OnNext(2);
+            receivedSignals.Count.Should().Be(1);
+
+            scheduler.AdvanceTo(35);
+            receivedSignals.Count.Should().Be(2);
+
+            scheduler.AdvanceTo(100);
+            receivedSignals.Count.Should().Be(2);
+        }
+
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void SimpleDelayWithDrop()
+        {
+            var receivedSignals = new List<int>();
+            var subject = new Subject<int>();
+            var scheduler = new TestScheduler();
+            subject.UniqueThrottle(TimeSpan.FromTicks(25), scheduler).Subscribe(x => receivedSignals.Add(x));
+
+
+            subject.OnNext(1);
+            subject.OnNext(1);
+            subject.OnNext(1);
+            receivedSignals.Count.Should().Be(0);
+            scheduler.AdvanceTo(24);
+            subject.OnNext(1);
+            subject.OnNext(1);
+            subject.OnNext(1);
+            receivedSignals.Count.Should().Be(0);
+
+            scheduler.AdvanceTo(25);
+            receivedSignals.Count.Should().Be(1);
+
+            scheduler.AdvanceTo(50);
+            subject.OnNext(1);
+            receivedSignals.Count.Should().Be(1);
+
+            scheduler.AdvanceTo(100);
+            receivedSignals.Count.Should().Be(2);
+        }
+    }
+
+    public class KeyedBufferObservableTests
+    {
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void SimpleDelay()
+        {
+            var receivedSignals = new List<int>();
+            var subject = new Subject<int>();
+            var scheduler = new TestScheduler();
+            subject.UniqueThrottle(25.Ticks(), scheduler).Subscribe(receivedSignals.Add);
+
+            subject.OnNext(1);
+            receivedSignals.Count.Should().Be(0);
+
+            scheduler.AdvanceBy(24);
+            receivedSignals.Count.Should().Be(0);
+
+            scheduler.AdvanceBy(1);
+            receivedSignals.Count.Should().Be(1);
+
+            scheduler.AdvanceBy(100);
+            receivedSignals.Count.Should().Be(1);
+        }
+
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void SimpleDelayWithDrop()
+        {
+            var receivedSignals = new List<int>();
+            var subject = new Subject<int>();
+            var scheduler = new TestScheduler();
+            subject.UniqueThrottle(25.Ticks(), scheduler).Subscribe(receivedSignals.Add);
+
+            subject.OnNext(1);
+            subject.OnNext(1);
+            subject.OnNext(1);
+            receivedSignals.Count.Should().Be(0);
+            scheduler.AdvanceBy(24);
+            subject.OnNext(1);
+            subject.OnNext(1);
+            subject.OnNext(1);
+            receivedSignals.Count.Should().Be(0);
+
+            scheduler.AdvanceBy(1);
+            receivedSignals.Count.Should().Be(1);
+
+            scheduler.AdvanceBy(100);
+            receivedSignals.Count.Should().Be(1);
+        }
+    }
+}
---
+++ Woeber.RxExtensions/UniqueThrottle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace Woeber.RxExtensions
+{
+    public class UniqueThrottle<T>
+    {
+        private readonly TimeSpan _delay;
+        private readonly IScheduler _scheduler;
+        private readonly Action<T> _sendSignal;
+        private readonly ConcurrentDictionary<T,int> _delayedSignals = new ConcurrentDictionary<T, int>(); 
+
+        public UniqueThrottle(TimeSpan delay, IScheduler scheduler, Action<T> sendSignal)
+        {
+            _delay = delay;
+            _scheduler = scheduler;
+            _sendSignal = sendSignal;
+        }
+
+        public void ReceiveInput(T input)
+        {
+            if (IsMonitored(input))
+                return;
+            BeginMonitor(input);
+        }
+
+        private bool IsMonitored(T input)
+        {
+            return _delayedSignals.ContainsKey(input);
+        }
+
+        private void BeginMonitor(T input)
+        {
+            _delayedSignals.TryAdd(input, 1);
+            ScheduleSignal(input);
+        }
+
+        private void ScheduleSignal(T input)
+        {
+            _scheduler.Schedule(_delay, () => Signal(input));
+        }
+
+        private void Signal(T input)
+        {
+            int _;
+            _delayedSignals.TryRemove(input, out _);
+            SendSignal(input);
+        }
+
+        private void SendSignal(T key)
+        {
+            _sendSignal(key);
+        }
+    }
+
+    public static class UniqueThrottleExtensionMethods
+    {
+        public static IObservable<T> UniqueThrottle<T>(this IObservable<T> observable, TimeSpan delay, IScheduler scheduler = default(IScheduler))
+        {
+            var keyedObservable = Observable.Create<T>(o =>
+            {
+                scheduler = scheduler ?? Scheduler.Default;
+                var keyedBuffer = new UniqueThrottle<T>(
+                    delay, 
+                    scheduler, 
+                    o.OnNext);
+                var subscription = observable.Subscribe(
+                    input => keyedBuffer.ReceiveInput(input), 
+                    o.OnError, 
+                    o.OnCompleted);
+                return () =>
+                {
+                    subscription.Dispose();
+                };
+            });
+            return keyedObservable;
+        }
+    }
+}
